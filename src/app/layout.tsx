e3da24c7fec8e60944b498cea545c9d2e6b18140
johnycaelum <johnycaelum@users.noreply.github.com>
2026-03-26 17:04:55 +0700

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export const metadata: Metadata = {
   title: "Партнёрская программа от Центра Банкротства Юрист — cbucompany.ru",
   description: "Партнёрская программа от Центра Банкротства Юрист cbucompany.ru — приглашайте клиентов на банкротство и зарабатывайте",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  icons: { icon: "/favicon.svg" },
 };
 
 export default function RootLayout({
